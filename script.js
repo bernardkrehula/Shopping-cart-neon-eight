021741540a5ef5491a6673a7d7f7a623935ba76c
bernardkrehula <krehula.bernard456@gmail.com>
2025-03-16 20:40:56 +0100

    Sort alphabetically by click

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const carContent = document.querySelector('.car-content');
 const main = document.querySelector('.main');
 const carsOnScreenDiv = document.querySelector('.cars');
 const availabilityBtn = document.querySelector('.availability');
+const sortOptions = document.querySelector('.sortOptions');
 
 const carCreator = (id, name, brand, manufacturedYear, doors, price, available, image) => {
     const getId = () => { return id };
@@ -24,21 +25,27 @@ function manageCarArray() {
         carsArray.push(car);
     }
     const sortArrayFromAtoZ = () => {
-        
+        carsArray.sort((a, b) => a.getName().localeCompare(b.getName()));
     }
+    const sortArrayFromZtoA = () => {
+        carsArray.sort((a, b) => b.getName().localeCompare(a.getName()));
+    }
+    const showCardsOnScreen = () => {
+        carsArray.map(car => pushCarsOnScreen(car));
+    }
+
     const returnArray = () => { return carsArray };
 
-    return { pushCarsInArray, returnArray, sortArrayFromAtoZ }
+    return { pushCarsInArray, sortArrayFromAtoZ, sortArrayFromZtoA, returnArray, showCardsOnScreen}
 }
 
 const manageCars = manageCarArray();
 
 for(let i = 0; i < cars.length; i++){
     const createCars = carCreator(cars[i].id, cars[i].name, cars[i].brand, cars[i].manufacturedYear , cars[i].doors , cars[i].price , cars[i].available, cars[i].image);
-    const getArrayProperties = manageCars.returnArray();
 
     manageCars.pushCarsInArray(createCars);
-    pushCarsOnScreen(getArrayProperties[i].getId(), getArrayProperties[i].getName(), getArrayProperties[i].getBrand(), getArrayProperties[i].getManufacturedYear(), getArrayProperties[i].getDoorsNumber(), getArrayProperties[i].getPrice(), getArrayProperties[i].isAvailable(), getArrayProperties[i].getImg());   
+    pushCarsOnScreen(createCars);
 }
 function backgroundColor(available) {
     if(available == 'yes'){
@@ -50,22 +57,22 @@ function backgroundColor(available) {
    
 }
 
-function pushCarsOnScreen(id, name, brand, manufacturedYear, doors, price, available, img ) {
+function pushCarsOnScreen(car) {
     const html = 
     `
-    <div class="car-content" id="${id}">
+    <div class="car-content" id="${car.getId()}">
             <div>
-                <h1>${name}</h1>
-                <img src="${img}">
+                <h1>${car.getName()}</h1>
+                <img src="${car.getImg()}">
             </div>
             <div class="car-description">
-                <h3>Brand: ${brand}</h3>
-                <h3>Manufactured year: ${manufacturedYear}</h3>
-                <h3>Doors: ${doors}</h3>
-                <h3>Price: $${price}</h3>
+                <h3>Brand: ${car.getBrand()}</h3>
+                <h3>Manufactured year: ${car.getManufacturedYear()}</h3>
+                <h3>Doors: ${car.getDoorsNumber()}</h3>
+                <h3>Price: $${car.getPrice()}</h3>
             </div>
-            <div class="available-info" style="background-color: ${backgroundColor(available)};">
-                <h3>Available: ${available}</h3>
+            <div class="available-info" style="background-color: ${backgroundColor(car.isAvailable())};">
+                <h3>Available: ${car.isAvailable()}</h3>
             </div>
             <button>Delete</button>
     </div>
@@ -73,10 +80,20 @@ function pushCarsOnScreen(id, name, brand, manufacturedYear, doors, price, avail
     carsOnScreenDiv.insertAdjacentHTML('beforeend', html);
 }
 
-availabilityBtn.addEventListener('click', () => {
-    // carsOnScreenDiv.innerHTML = '';
-    manageCars.sortArrayFromAtoZ();
-      
+sortOptions.addEventListener('change', () => {
+    const az = sortOptions.value;
+
+    if(az == 'sort-a-z' ){
+        carsOnScreenDiv.innerHTML = '';
+        manageCars.sortArrayFromAtoZ();
+        manageCars.showCardsOnScreen();
+    }
+    if(az == 'sort-z-a'){
+        carsOnScreenDiv.innerHTML = '';
+        manageCars.sortArrayFromZtoA();
+        manageCars.showCardsOnScreen();
+    }
+    
 })
 carsOnScreenDiv.addEventListener('click', (e) => {
     const deleteBtn = e.target.closest('button');
