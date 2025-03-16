a67d7cd7f0b06d0e12f4d8018d58897ae593381c
bernardkrehula <krehula.bernard456@gmail.com>
2025-03-16 15:00:04 +0100

    Push cars on screen

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import { cars } from "./carArray.js";
 
 const carContent = document.querySelector('.car-content');
+const main = document.querySelector('.main');
+const carsOnScreenDiv = document.querySelector('.cars');
 
 const carCreator = (id, name, brand, manufacturedYear, doors, price, available, image) => {
     const getId = () => { return id };
@@ -21,7 +23,7 @@ function manageCarArray() {
         carsArray.push(car)
     }
     const returnArray = () => { return carsArray };
-    
+
     return { pushCarsInArray, returnArray }
 }
 const manageCars = manageCarArray();
@@ -29,5 +31,29 @@ const manageCars = manageCarArray();
 for(let i = 0; i < cars.length; i++){
     const createCars = carCreator(cars[i].id, cars[i].name, cars[i].brand, cars[i].manufacturedYear , cars[i].doors , cars[i].price , cars[i].available, cars[i].image);
     manageCars.pushCarsInArray(createCars);
+    pushCarsOnScreen();
+}
+
+function pushCarsOnScreen() {
+    const html = 
+    `
+    <div class="car-content">
+            <div>
+                <h1>Toyota Corrolla</h1>
+                <img src="toyota-corrolla.png">
+            </div>
+            <div class="car-description">
+                <h3>Brand: Toyota</h3>
+                <h3>Manufactured year: 2019</h3>
+                <h3>Doors: 4</h3>
+                <h3>Price: $22000</h3>
+            </div>
+            <div class="available-info">
+                <h3>Available: Yes</h3>
+            </div>
+            <button>Delete</button>
+    </div>
+    `
+    carsOnScreenDiv.insertAdjacentHTML('beforeend', html);
 }
 console.log(manageCars.returnArray())
