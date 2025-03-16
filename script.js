bed1a6632e4635e94a42e7127de6bf40be0bf18c
bernardkrehula <krehula.bernard456@gmail.com>
2025-03-16 16:22:09 +0100

    Change Available based on poperties value

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const carContent = document.querySelector('.car-content');
 const main = document.querySelector('.main');
 const carsOnScreenDiv = document.querySelector('.cars');
 
+
 const carCreator = (id, name, brand, manufacturedYear, doors, price, available, image) => {
     const getId = () => { return id };
     const getName = () => { return name };
@@ -31,11 +32,21 @@ const manageCars = manageCarArray();
 for(let i = 0; i < cars.length; i++){
     const createCars = carCreator(cars[i].id, cars[i].name, cars[i].brand, cars[i].manufacturedYear , cars[i].doors , cars[i].price , cars[i].available, cars[i].image);
     const getArrayProperties = manageCars.returnArray();
+
     manageCars.pushCarsInArray(createCars);
-    pushCarsOnScreen(getArrayProperties[i].getId(), getArrayProperties[i].getName(), getArrayProperties[i].getBrand(), getArrayProperties[i].getManufacturedYear(), getArrayProperties[i].getDoorsNumber(), getArrayProperties[i].getPrice(), getArrayProperties[i].isAvailable(), getArrayProperties[i].getImg());
+    pushCarsOnScreen(getArrayProperties[i].getId(), getArrayProperties[i].getName(), getArrayProperties[i].getBrand(), getArrayProperties[i].getManufacturedYear(), getArrayProperties[i].getDoorsNumber(), getArrayProperties[i].getPrice(), getArrayProperties[i].isAvailable(), getArrayProperties[i].getImg());   
+}
+function backgroundColor(available) {
+    if(available == 'yes'){
+        return 'rgb(128, 255, 128)';
+    }
+    if(available == 'no') {
+        return 'rgb(240, 61, 61)';
+    }
+   
 }
 
-function pushCarsOnScreen(id, name, brand, manufacturedYear, doors, price, available, img) {
+function pushCarsOnScreen(id, name, brand, manufacturedYear, doors, price, available, img ) {
     const html = 
     `
     <div class="car-content" id="${id}">
@@ -47,9 +58,9 @@ function pushCarsOnScreen(id, name, brand, manufacturedYear, doors, price, avail
                 <h3>Brand: ${brand}</h3>
                 <h3>Manufactured year: ${manufacturedYear}</h3>
                 <h3>Doors: ${doors}</h3>
-                <h3>Price: ${price}</h3>
+                <h3>Price: $${price}</h3>
             </div>
-            <div class="available-info">
+            <div class="available-info" style="background-color: ${backgroundColor(available)};">
                 <h3>Available: ${available}</h3>
             </div>
             <button>Delete</button>
